8a3871d973222270e97bafb82c64cb137560dee4
luchangqing <525601201@qq.com>
2019-03-06 18:11:31 +0800

    Initial commit

---
+++ src/main/java/com/luchangqing/sc_02_netflix_eureka/Sc02NetflixEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.luchangqing.sc_02_netflix_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sc02NetflixEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sc02NetflixEurekaApplication.class, args);
+	}
+
+}
